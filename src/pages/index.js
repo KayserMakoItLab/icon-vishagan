32d6afb2b72d888e4fbadc6809096a2573f29977
karthiktaff <karthik@taffinc.com>
2023-10-18 13:32:22 +0530

    updated icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
             <img className="entry-model" width={110} style={{ position: 'absolute', top: '185px', left: '245px' }} src='/assets/images/calculator.png' />
             <img className="entry-model" width={110} style={{ position: 'absolute', top: '-430px', right: '245px' }} src='/assets/images/camera.png' />
             <img className="entry-model" width={110} style={{ position: 'absolute', top: '-225px', right: '375px' }} src='/assets/images/chat.png' />
-            <img className="entry-model" width={110} style={{ position: 'absolute', top: '10px', right: '350px' }} src='/assets/images/clock.png' />
+            <img className="entry-model" width={110} style={{ position: 'absolute', top: '10px', right: '350px' }} src='/assets/images/Clock.png' />
             <img className="entry-model" width={110} style={{ position: 'absolute', top: '185px', right: '245px' }} src='/assets/images/coconut.png' />
             <img className="entry-model" width={110} style={{ position: 'absolute', top: '-345px', left: '530px' }} src='/assets/images/coffee_cup.png' />
             <img className="entry-model" width={110} style={{ position: 'absolute', top: '-100px', left: '570px' }} src='/assets/images/coffee_mug.png' />
